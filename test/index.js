e8c06f85bbbf48e79ad454dee8e0fadf8ee19b75
Petr Pchelko <ppchelko@wikimedia.org>
2016-10-03 15:31:50 -0700

    ES6, ESLint

--- test/index.js
+++ test/index.js
@@ -5,3 +5,5 @@
 require('mocha-jshint')();
 // Run jscs as part of normal testing
 require('mocha-jscs')();
+// Run eslint as part of normal testing
+require('mocha-eslint')([ './lib', 'index.js' ]);

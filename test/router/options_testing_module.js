193e048c7e715436406b5d29200d8fd22ae9f0cf
Petr Pchelko <ppchelko@wikimedia.org>
2018-05-02 16:21:40 -0300

    Removed backwards-compatible options.log, hyper.log

--- test/router/options_testing_module.js
+++ test/router/options_testing_module.js
@@ -6,7 +6,7 @@ module.exports = function(options) {
     assert.deepEqual(options.simple_option, 'simple_option_value');
     assert.deepEqual(options.templated_option, 'test_conf_option_value');
     assert.deepEqual(options.templates, { sample_template: '{{should not be expanded}}' });
-    assert.deepEqual(typeof options.log, 'function');
+    assert.deepEqual(!!options.logger, true );
     return {
         spec: {
             paths: {
